970bd41163b9863f35ed237ae01d40bb5dffd95a
Admin <mykhaylopelykh@gmail.com>
2022-11-10 16:47:14 +0100

    finish navbar and start design categorie

---
+++ src/Components/Categories.js
@@ -0,0 +1,106 @@
+import React from 'react'
+
+function Categories() {
+  return (
+    <div class="bg-indigo-50">
+        <section class="min-h-screen body-font text-gray-600 ">
+            <div class="container mx-auto px-5 py-10">
+                <div class="-m-4 flex flex-wrap">
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                    <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                        <a class="relative block h-48 overflow-hidden rounded">
+                            <img alt="ecommerce" class="block h-full mx-auto object-cover object-center cursor-pointer" src="https://cdn.pixabay.com/photo/2017/08/27/23/59/marvel-2688068_960_720.jpg" />
+                        </a>
+                        <div class="mt-4">
+                            <h2 class="title-font text-lg font-medium text-gray-900">Titre film</h2>
+                            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">Catégorie</h3>
+                            <p class="mt-1">2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Categories
