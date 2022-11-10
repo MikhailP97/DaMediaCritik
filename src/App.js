394a67ab9f88f05a41518474247ec1532cc81e34
Thierry ZENNARO <tzen@hotmail.fr>
2022-11-10 10:40:55 +0100

    Commit Components

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ import FilmsParCategorie from "../src/Views/FilmsParCategorie.js";
 import PageFilm from "../src/Views/PageFilm.js";
 import Profile from "../src/Views/Profile.js";
 
-import Tendances from './components/Tendances.js'
-import Footer from './components/Footer.js'
+import Footer from './Components/Footer.js'
 
 function App() {
   return (
@@ -34,8 +33,6 @@ function App() {
                 </Routes>
 
       </BrowserRouter>
-
-      <Tendances/>
 
       <Footer/>
     </div>
