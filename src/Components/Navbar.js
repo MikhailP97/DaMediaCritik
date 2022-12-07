ba7cf1d5e8ea5e5dc45e1401242469585697bcdf
Thierry ZENNARO <tzen@hotmail.fr>
2022-12-08 00:33:51 +0100

    Commit du 08-12-2022

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,236 +1,200 @@
-import React, { useState } from 'react';
-import {  useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    //navigation
-    const navigate = useNavigate();
+  //navigation
+  const navigate = useNavigate();
 
-    return (
-      <div className="bg-stone-700">
-        <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl w-full md:px-24 lg:px-8">
-          <div className="relative flex grid items-center grid-cols-3 lg:grid-cols-4">
-
-            {/* Title */}
-
-            <div
-              onClick={() => navigate("/")}
-              className="inline-flex items-center "
-            >
-              <span className="ml-2 text-xl font-bold tracking-wide text-amber-600">
-                DaMovieCritik
-              </span>
-            </div>
-
-            {/* Search Bar */}
-
-            <div className='max-w-md mx-auto invisible lg:visible w-full'>
-                <div className="border border-white relative flex items-center  h-12 rounded-full focus-within:shadow-lg bg-stone-700 overflow-hidden">
-                    <input
-                    className="peer h-full w-full outline-none text-sm text-gray-100 bg-stone-700 pr-2 ml-2"
-                    type="text"
-                    id="search"
-                    placeholder="Recherche" /> 
-                    <div className="grid place-items-center h-full w-12 bg-stone-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                    </div>
-                </div>
-            </div>
-
-            {/* Menu Links */}
-
-            <ul className="flex items-center hidden lg:ml-5 ml-auto space-x-8 lg:flex">
-              <li>
-                <div
-                  onClick={() => navigate("/")}
-                  aria-label="Sign in"
-                  title="Home"
-                  className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
-                >
-                  Accueil
-                </div>
-              </li>
-              <li>
-                <div
-                  onClick={() => navigate("/films")}
-                  className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
-                  aria-label="Films"
-                  title="Films"
-                >
-                  Films
-                </div>
-              </li>
-              <li>
-                <div
-                  onClick={() => navigate("/films-par-categorie")}
-                  className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
-                  aria-label="Catégories"
-                  title="Catégories"
-                >
-                  Catégories
-                </div>
-              </li>
-              <li>
-                <div
-                  onClick={() => navigate("/contact")}
-                  className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
-                  aria-label="Sign up"
-                  title="Sign up"
-                >
-                  Contact
-                </div>
-              </li>
-              <li>
-                <div
-                    onClick={() => navigate("/profile")}
-                    className="inline-flex items-center lg:mx-auto"
-                    >
-                    <svg className="w-6 h-6 text-amber-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
-                </div>
-                    
-                
-              </li>
-            </ul>
-            
-            
-
-            {/* Sandwich Menu */}
-
-            <div className="ml-auto sm:hidden">
-              <button
-                aria-label="Open Menu"
-                title="Open Menu"
-                className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
-                onClick={() => setIsMenuOpen(true)}
+  return (
+    <div>
+      <nav className="w-full bg-stone-700 shadow py-2">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 max-h-16">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              <div
+                onClick={() => navigate("/")}
+                class="inline-flex items-center "
               >
-                <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                  <path
-                    fill="currentColor"
-                    d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-                  />
-                  <path
-                    fill="currentColor"
-                    d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-                  />
-                  <path
-                    fill="currentColor"
-                    d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-                  />
-                </svg>
-              </button>
-              {isMenuOpen && (
-                <div className="absolute top-0 left-0 w-full">
-                  <div className="p-5 bg-white border rounded shadow-sm">
-                    <div className="flex items-center justify-between mb-4">
-                      <div>
-                        <a
-                          href="/"
-                          aria-label="Company"
-                          title="Company"
-                          className="inline-flex items-center"
-                        >
-                          <svg
-                            className="w-8 text-deep-purple-accent-400"
-                            viewBox="0 0 24 24"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeMiterlimit="10"
-                            stroke="currentColor"
-                            fill="none"
+                <span class="ml-2 text-xl font-bold tracking-wide text-amber-600">
+                  DaMovieCritik
+                </span>
+              </div>
+              <div class="ml-auto md:hidden">
+                <button
+                  aria-label="Open Menu"
+                  title="Open Menu"
+                  class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                  onClick={() => setIsMenuOpen(true)}
+                >
+                  <svg class="w-5 text-gray-100" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                    />
+                    <path
+                      fill="currentColor"
+                      d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                    />
+                    <path
+                      fill="currentColor"
+                      d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                    />
+                  </svg>
+                </button>
+                {isMenuOpen && (
+                  <div class="absolute top-0 left-0 w-full">
+                    <div class="p-5 bg-stone-700 border rounded shadow-sm">
+                      <div class="flex items-center justify-between mb-4">
+                        <div>
+                          <button
+                            aria-label="Close Menu"
+                            title="Close Menu"
+                            class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:shadow-outline"
+                            onClick={() => setIsMenuOpen(false)}
                           >
-                            <rect x="3" y="1" width="7" height="12" />
-                            <rect x="3" y="17" width="7" height="6" />
-                            <rect x="14" y="1" width="7" height="6" />
-                            <rect x="14" y="11" width="7" height="12" />
-                          </svg>
-                          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
-                            DaMovieCritik
-                          </span>
-                        </a>
+                            <svg class="w-5 text-gray-100" viewBox="0 0 24 24">
+                              <path
+                                fill="currentColor"
+                                d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                              />
+                            </svg>
+                          </button>
+                        </div>
                       </div>
-                      <div>
-                        <button
-                          aria-label="Close Menu"
-                          title="Close Menu"
-                          className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                          onClick={() => setIsMenuOpen(false)}
-                        >
-                          <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                            <path
-                              fill="currentColor"
-                              d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-                            />
-                          </svg>
-                        </button>
-                      </div>
+                      <nav>
+                        <ul class="space-y-4">
+                          <li className="text-gray-100">
+                            <div
+                              onClick={() => {
+                                setIsMenuOpen(false);
+                                navigate("/");
+                              }}
+                            >
+                              Accueil
+                            </div>
+                          </li>
+                          <li className="text-gray-100">
+                            <div
+                              onClick={() => {
+                                setIsMenuOpen(false);
+                                navigate("/films");
+                              }}
+                            >
+                              Films
+                            </div>
+                          </li>
+                          <li className="text-gray-100">
+                            <div
+                              onClick={() => {
+                                setIsMenuOpen(false);
+                                navigate("/films-par-categorie");
+                              }}
+                            >
+                              Catégories
+                            </div>
+                          </li>
+                          <li className="text-gray-100">
+                            <div
+                              onClick={() => {
+                                setIsMenuOpen(false);
+                                navigate("/contact");
+                              }}
+                            >
+                              Contact
+                            </div>
+                          </li>
+                          <li className="text-gray-100">
+                            <div
+                              onClick={() => {
+                                setIsMenuOpen(false);
+                                navigate("/profile");
+                              }}
+                            >
+                              Profile
+                            </div>
+                          </li>
+                        </ul>
+                      </nav>
                     </div>
-                    <nav>
-                      <ul className="space-y-4">
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Our product"
-                            title="Our product"
-                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Product
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Our product"
-                            title="Our product"
-                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Features
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Product pricing"
-                            title="Product pricing"
-                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Pricing
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Sign in"
-                            title="Sign in"
-                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Sign in
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                            aria-label="Sign up"
-                            title="Sign up"
-                          >
-                            Sign up
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
                   </div>
-                </div>
-              )}
+                )}
+              </div>
             </div>
           </div>
+
+          {/* Search Bar */}
+
+          <div class="max-w-md invisible lg:mx-20 lg:visible w-full">
+            <div class="border border-white relative flex items-center  max-h-10 h-8 rounded-full focus-within:shadow-lg bg-stone-700 overflow-hidden">
+              <input
+                class="peer h-full w-full outline-none text-sm text-c text-gray-100 bg-stone-700 pr-2 ml-2"
+                type="text"
+                id="search"
+                placeholder="Recherche"
+              />
+              <div class="grid place-items-center h-full w-12 bg-stone-700">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 text-gray-100"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0  block`}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="transition ease-in-out origin-center delay-75 text-gray-100 hover:text-amber-600 hover:scale-125">
+                <div onClick={() => navigate("/")}>Accueil</div>
+              </li>
+              <li className="transition ease-in-out origin-center delay-75 text-gray-100 hover:text-amber-600 hover:scale-125">
+                <div onClick={() => navigate("/films")}>Films</div>
+              </li>
+              <li className="transition ease-in-out origin-center delay-75 text-gray-100 hover:text-amber-600 hover:scale-125">
+                <div onClick={() => navigate("/categories")}>Catégories</div>
+              </li>
+              <li className="transition ease-in-out origin-center delay-75 text-gray-100 hover:text-amber-600 hover:scale-125">
+                <div onClick={() => navigate("/contact")}>Contact</div>
+              </li>
+              <li>
+                <div onClick={() => navigate("/profile")}>
+                  <svg
+                    class="h-6 w-6 text-gray-100 hover:fill-current hover:text-amber-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                    />
+                  </svg>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    );
+      </nav>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
