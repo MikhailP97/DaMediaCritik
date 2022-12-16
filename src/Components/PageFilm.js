04b923e74cc2fab99cb3583e850a7421277c6945
Admin <pierre.alves.317@gmail.com>
2022-12-16 10:04:03 +0100

    commit 16/12

--- src/Components/PageFilm.js
+++ src/Components/PageFilm.js
@@ -5,23 +5,52 @@ import { getAsyncMoviePage, moviePage } from '../features/movies/movieSlice';
 
 function PageFilm() {
 
-    //get url
-    const { id } = useParams();
-    console.log(id);
+  //get url
+  const { id } = useParams();
+  console.log(id);
 
-    const movieData = useSelector(moviePage);
-    const dispatch = useDispatch();
+  const movieData = useSelector(moviePage);
+  const dispatch = useDispatch();
 
-    console.log(movieData)
+  console.log(movieData)
 
-    useEffect(() => {
-        dispatch(getAsyncMoviePage(id))
-    }, [])
+  useEffect(() => {
+    dispatch(getAsyncMoviePage(id))
+  }, [])
 
   return (
-    <div style={{color: 'red', fontSize: 50}}>{movieData.title}
-    
-    <img src={`https://image.tmdb.org/t/p/original/${movieData.poster_path}`} alt={movieData.title}/>
+    <div>
+      <div className=" mt-5 relative flex py-2 items-center">
+        <div className="flex-grow border-t ml-20 border-amber-200"></div>
+        <span className="flex-shrink my-10 mx-4 text-amber-500 text-3xl font-bold">{movieData.title}</span>
+        <div className="flex-grow border-t mr-20 border-amber-200"></div>
+      </div>
+ 
+        <img className="sm:absolute w-full " src={`https://image.tmdb.org/t/p/original/${movieData.poster_path}`} alt={movieData.title} />
+
+        <div>
+
+        <div className=" sm:grid sm:grid-cols-2 relative bg-black bg-opacity-90   sm:my-10 md:my-24 lg:my-48  sm:mx-8 md:mx-10 lg:mx-20 px-20 py-5 sm:py-10 md:py-20 lg:py-40 rounded-xl  text-white">
+          <div className="block">
+
+            <p className="text-4xl ">Titre : </p>
+            <p className="text-4xl pt-3 pb-10  text-amber-500  ">{movieData.title}</p>
+
+            <p className="text-3xl ">Date de sortie : </p>
+            <p className="text-3xl pt-3 pb-10 text-amber-500  ">{movieData.release_date}</p>
+            </div>
+            <div className="col-span-2 text-3xl ">Description : 
+            <p className="text-xl pr-5">
+        "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+        </p>
+        </div>
+        
+
+        <img className="hidden  sm:block" src={`https://image.tmdb.org/t/p/original/${movieData.poster_path}`} alt={movieData.title}/>
+        </div>
+        </div>
+
+
 
     </div>
   )
--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -6,56 +6,99 @@ export default function Profile() {
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-y-10">
-        <div id="pseudo" className="text-center col-span-3 text-4xl text-amber-500  py-20  border-amber-200  border-b">Bievenue "Pseudonyme récupéré dynamiquement" </div>
+     <div>
+        <div className=" flex flex-col lg:flex lg:flex-row lg:justify-center py-20 " ><img className="mt-10 m-auto lg:m-0" id="avatar" src="Images/sheep-7624863_1920.jpg" alt="mouton" />
+          <div id="pseudo" className="text-center lg:ml-40 lg:pb-0 text-4xl text-amber-100  pt-24 pb-10  border-amber-200  border-b-8">Bievenue <span className="text-amber-500 font-bold">John Doe</span> </div>
+        </div>
         {/* En attendant de faire la vraie fonction pour se déconnecter (factice) */}
 
-        <div className="flex flex-col justify-center text-amber-100 py-10 ml-40 ">
-          <p className=" text-2xl underline">Favoris</p>
-          <br />
-          <div className="grid grid-cols-3 gap-10">
-           <div>
-              <img id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 1 </p>
-            </div>
-           
-            <div>
-              <img id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 1 </p>
-            </div>
-          
-            <div>
-              <img id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 3 </p>
-            </div>
 
-            <div>
-              <img id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 3 </p>
-            </div>
-
-            <div>
-              <img id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 3 </p>
-            </div>
-
-            <div>
-              <img id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 3 </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-wrap justify-center content-center py-10 " ><img id="avatar" src="Images/sheep-7624863_1920.jpg" alt="mouton" /></div>
-
-        <div className="flex flex-col justify-center text-amber-100 py-10 ml-40">
-          <p className="text-2xl underline">Infos</p>
+        <div className="flex flex-col text-center lg:text-left text-amber-100 py-10 lg:ml-40">
+          <p className="text-3xl underline">Infos</p>
           <br />
           <div className="flex flex-col space-y-1">
-            <p className="text-lg">Date de naissance : DATE</p>
-            <p className="text-lg">Adresse mail : EMAIL</p>
-            <p className="text-lg">Adresse mail : EMAIL</p>
+          <p className="text-lg">Pseudo :  <span className="text-amber-500">John Doe</span></p>
+            <p className="text-lg">Date de naissance : <span className="text-amber-500">JJ/MM/AAA</span> </p>
+            <p className="text-lg">Adresse mail : <span className="text-amber-500">john.doe@mail.com</span></p>
+            
 
-          </div>
+
 
         </div>
 
-        <div className="flex justify-center col-span-3 border-amber-200 border-t">
-          <button onClick={() => navigate('/login')} className="mb-20 shadow-md shadow-stone-300/50 bg-stone-900 py-1 px-5 mt-20 rounded-md text-lg text-white font-semibold border-2 border-white hover:text-amber-300 hover:border-amber-300 hover:shadow-amber-300/50  ">Se déconnecter</button>
+        </div>
+          <div className="m-auto w-1/4 mt-20 mb-10 border-b-4 border-amber-200"></div>
+
+
+        <div className="flex flex-col text-amber-100 py-10 ">
+
+          <p className=" text-3xl underline m-auto lg:ml-40 pb-5">Favoris</p>
+          <br />
+          <div className=" grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 lg:mx-40 xl:grid-cols-4 mx-20 gap-20 ">
+            <div>
+              <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris  </p>
+            </div>
+
+            <div>
+              <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris  </p>
+            </div>
+
+            <div>
+              <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris  </p>
+            </div>
+
+            <div>
+              <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris  </p>
+            </div>
+
+            <div>
+              <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris  </p>
+            </div>
+
+            <div>
+              <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris  </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="m-auto w-1/4 mt-20 mb-10 border-b-4 border-amber-200"></div>
+
+
+<div className="flex flex-col text-amber-100 py-10 ">
+
+  <p className=" text-3xl underline m-auto lg:ml-40 pb-5"><span className=" text-amber-500">Critiks</span></p>
+  <br />
+  <div className=" grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 lg:mx-40 xl:grid-cols-4 mx-20 gap-20 ">
+    <div>
+      <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Film </p>
+    </div>
+
+    <div>
+      <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Film </p>
+    </div>
+
+    <div>
+      <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 3 </p>
+    </div>
+
+    <div>
+      <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 3 </p>
+    </div>
+
+    <div>
+      <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 3 </p>
+    </div>
+
+    <div>
+      <img className="rounded-md" id="favoris" src="Images/sheep-7624863_1920.jpg" alt="affiche de film" /><p className="text-lg">Favoris 3 </p>
+    </div>
+  </div>
+</div>
+
+<div className="m-auto w-1/4 mt-20 border-b-4 border-amber-200"></div>
+
+        <div className="flex justify-center col-span-3 ">
+          <button onClick={() => navigate('/login')} className="mb-20 shadow-md shadow-stone-300/50 bg-stone-900 py-3 lg:py-1 px-5 lg:px-5 mt-20 rounded-md text-lg text-white font-semibold border-2 border-white hover:text-amber-300 hover:border-amber-300 hover:shadow-amber-300/50  ">Se déconnecter</button>
         </div>
       </div>
     </>
